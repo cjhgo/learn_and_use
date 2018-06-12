94a773927a9cc3748e6ffcdc222cb2a9d179142e
chen <1728533217en@gmail.com>
2018-06-12 23:20:07 +0800

    update

--- cpp/stl/usevector.cpp
+++ cpp/stl/usevector.cpp
@@ -3,11 +3,18 @@
 int main(int argc, char const *argv[])
 {
 	std::vector<double> v;
-	v.push_back(5);
-	v.push_back(2.0);
+	v.resize(4,0);
+	for(double e : v)
+	{
+		std::cout<<e<<std::endl;
+	}	
+	v={0,1,3,4};
 	for(double e : v)
 	{
 		std::cout<<e<<std::endl;
 	}
+	std::cout<<v.size();
+	v.clear();
+	std::cout<<v.size();
 	return 0;
 }
