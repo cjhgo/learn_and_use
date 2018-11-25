79d63092a0e5c338deae7e1130de388c5c947988
chen <1728533217en@gmail.com>
2018-11-25 23:53:27 +0800

    update

--- cpp/atomics/Bit_manipulation.cpp
+++ cpp/atomics/Bit_manipulation.cpp
@@ -1,5 +1,11 @@
+#include<iostream>
+#include<bitset>
+using namespace std;
 int main(int argc, char const *argv[])
 {
-    
+    bitset<8> t(34);
+    cout<<t<<endl;
+    t=30;
+    cout<<t<<endl;
     return 0;
 }
