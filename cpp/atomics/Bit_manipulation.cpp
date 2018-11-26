112e026b3fe12a676f4a9c034f2e58666b19d9c2
chen <1728533217en@gmail.com>
2018-11-26 12:20:10 +0800

    update'

--- cpp/atomics/Bit_manipulation.cpp
+++ cpp/atomics/Bit_manipulation.cpp
@@ -18,5 +18,15 @@ int main(int argc, char const *argv[])
     cout<<"test bit at pos 5"<<endl;
     t=(34 & (1<<5));
     cout<<t<<endl;
+    auto a = 0b1010 & 0b0111;
+    auto b = 0b1010 && 0b0111;
+    cout<< a<<endl;//bitwise and -> 0b0010, 2
+    cout<< b<<endl;//logic and ->1
+    int y = 0;
+    cout<<(0 & (++y))<<endl;
+    cout<<y<<endl;
+    int z = 0;
+    cout<<(0 && (++z))<<endl;
+    cout<<z<<endl;
     return 0;
 }
