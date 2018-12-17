db960f6faec3f7a16eda71a0ec634758fd66591f
chen <1728533217en@gmail.com>
2018-12-17 22:19:10 +0800

    update

--- cpp/use_eigen/use_eigen.cpp
+++ cpp/use_eigen/use_eigen.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
+#include <vector>
 #include <Eigen/Dense>
 
-using Eigen::MatrixXd;
+using Eigen::MatrixXi;
+typedef Eigen::Matrix< int , 10, 1> Vector_type;
 
 int main(int argc, char const *argv[])
 {
-    MatrixXd m(2,2);
+    MatrixXi m(2,2);
     m(0,0)=3;
     m(1,0)=2.5;
     m(0,1)=1;
     m(1,1)=m(1,0)+m(0,1);
-    std::cout<<m<<std::endl;
+    std::cout<<m<<std::endl<<std::endl;
+    Vector_type w,x;
+    Eigen::Vector4d a(1,2,3,4), b(1,1,1,1);
+    std::cout<<a.dot(b)<<std::endl;
+    w=Eigen::MatrixXi::Constant(10,1,2);
+    x=Eigen::MatrixXi::Constant(10,1,1);
+    std::cout<<w.transpose()<<std::endl<<std::endl;
+    std::cout<<x.transpose()<<std::endl<<std::endl;
+    std::cout<<w.dot(x)<<std::endl<<std::endl;
     return 0;
 }
