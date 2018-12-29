49292430148d3a41d0fc6c351a910400047bcc95
chen <1728533217en@gmail.com>
2018-12-30 00:33:04 +0800

    update

--- shell/normal_use.sh
+++ shell/normal_use.sh
@@ -2,8 +2,18 @@
 
 : <<"eof"
 任务 计算程序执行时间
+用date +%s
+脚本中有一个预定义的变量
+记录了脚本从执行到目前为止,用的时间
 eof
 begin=$(date +%s)
-end=$(date +%s)
+#echo $SECONDS
+echo $SECONDS
+sleep 5;
 #run some long program
+end=$(date +%s)
 total_run=$(($end-$begin))
+
+echo $total_run
+sleep 10;
+echo $SECONDS
