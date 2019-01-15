fe1d063cc33bcc73a852bf8a4d0d4d5ff01a03b7
chen <1728533217en@gmail.com>
2019-01-15 13:47:29 +0800

    update

--- cpp/pthread/use_pthread_barrier.cpp
+++ cpp/pthread/use_pthread_barrier.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<pthread.h>
+#include<thread>
 
 static pthread_barrier_t barrier;
 void * foo(void* threadid)
@@ -10,7 +11,15 @@ void * foo(void* threadid)
     std::cout<<tid+10<<std::endl;
 }
 
-int main(int argc, char const *argv[])
+void bar(int n)
+{
+    std::cout<<"the n is :"<<n<<std::endl;
+    pthread_barrier_wait(&barrier);
+    std::cout<<"the n + 10 is :"<<n+10<<std::endl;
+}
+
+
+void use_pthread()
 {
     pthread_t t1,t2;
     pthread_barrier_init(&barrier, NULL, 2);
@@ -18,5 +27,18 @@ int main(int argc, char const *argv[])
     pthread_create(&t2, NULL, foo, (void *)2);
     pthread_join(t1, NULL);
     pthread_join(t2, NULL);
+}
+
+void use_stdthread()
+{
+    pthread_barrier_init(&barrier, NULL, 2);
+    std::thread t(bar, 55), s(bar, 20);
+    t.join();
+    s.join();
+}
+int main(int argc, char const *argv[])
+{
+    use_pthread();
+    use_stdthread();
     return 0;
 }
