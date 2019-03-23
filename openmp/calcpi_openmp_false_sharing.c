38e7913c7876b0f79700ccac2fc7a8109d32ec0c
chen <1728533217en@gmail.com>
2019-03-23 21:35:49 +0800

    update

---
+++ openmp/calcpi_openmp_false_sharing.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <omp.h>
+#define MAX_THREADS 4
+//$\int_{0}^{1} \frac{4.0}{\left(1+x^{2}\right)} d x=\pi$
+
+static int number_step=100000000;
+
+int main(int argc, char*argv[])
+{
+  
+  double realPI=3.141592653589793238462643;
+  double step = 1.0/number_step;
+    
+  for(size_t j = 1 ; j <= MAX_THREADS; j++)
+  {
+    double start_time=omp_get_wtime();
+    omp_set_num_threads(j);
+    double full_sum = 0;
+    double* partial_sum_array=(double*)malloc(j*sizeof(double));
+    #pragma omp parallel private(j)
+    {
+      int nthreads = omp_get_num_threads();
+      #pragma omp single
+      printf("the number of threads is :%d\n", nthreads);
+
+      int id = omp_get_thread_num();
+      double partial_sum = 0, x=0;
+      
+      for(j = id; j < number_step; j=j+nthreads)
+      { 
+        x = (j+0.5)*step;
+        partial_sum_array[id] += 4.0/(1+x*x);
+      }
+    }
+    for(size_t k = 0; k < j; k++)
+    {
+      full_sum += partial_sum_array[k]*step;      
+    }
+    printf("the res is: %.60f\n",full_sum);
+    printf("the  pi is: %.60f\n",realPI);
+    printf("spend time %.8f\n", omp_get_wtime()-start_time);
+  }
+
+}
