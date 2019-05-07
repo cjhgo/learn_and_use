274c4a8f0a3489ec9ebd4ad8dd97562145febdc3
chen <1728533217en@gmail.com>
2019-05-07 23:06:43 +0800

    update

--- cpp/c++primer/use_uniqueptr.cpp
+++ cpp/c++primer/use_uniqueptr.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-  unique_ptr<string> p1(new string("abcedf"));
+  auto x = new string("abcedf");
+  cout<<*x<<"\t"<<x<<endl;
+  unique_ptr<string> p1(x);
   unique_ptr<string> p2,p3;
-  cout<<*p1.get()<<endl;
+  cout<<*p1<<"\t"<<p1.get()<<endl;  
   p2 = std::move(p1);
-  cout<<*p2.get()<<endl;
+  cout<<*p2<<"\t"<<p2.get()<<endl;  
   p3.reset(p2.release());
-  cout<<*p3.get()<<endl;
+  cout<<*p3<<"\t"<<p3.get()<<endl;  
   return 0;
 }
