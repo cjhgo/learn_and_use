0176cb35274078c0c79c50bd39c4748bc336dfdc
chen <1728533217en@gmail.com>
2019-06-02 19:59:05 +0800

    update

--- cpp/about_init.cpp
+++ cpp/about_init.cpp
@@ -10,48 +10,57 @@ typedef struct Bar
     Bar(int bb):b(bb){};
 }Bar;
 
-class Foo
+
+void funfoo()
 {
-public:       
-    const static int a = 1;    
-    enum { arrsize = 2 };//
-    const static int  b[2];
-    constexpr static int c[2] = {1,2};
+    int array[]={1,3,4};
+    int a,b;
+    cout<<"!!!"<<array[0]<<endl;
+}
 
-    //error for 必须in-class initialization
-    //constexpr static int d[2]; 
-
-    //error for 必须是const
-    //static int e = 3;
-
-    //non-const-integral,non enum,non literaltype,不能in-class initialization
-    const static int f[2];
-    //non-static array ,since c++11 ,可以 in-class initialization
-    //但是,必须指明size
-    //在函数中,int z[]={1,2,3}是允许的
-    int z[3]={1,2,3};
-
-    vector<int> v{1,2,3,4};//since c++11
-    static vector<int> vv;
-    const static vector<int> vvv;
-    //不能这么写,Bar yourbar(33);因为这会被优先处理为一个函数声明
-    Bar yourbar = Bar(33);
-    Bar mybar{33};//since c++11
+class Fuu
+{
+public:
+    int array[3]={3,5,7};
+    int n = 99;
+    int ba{44};
+    const static int  bb[2];//static的const int[2]不能类内初始化={3,9};
+    constexpr static int d[2]={3,9}; //static constexpr必须类内初始化
+    static const int a = 1;    
+    enum { arrsize = 2 };
+    static const char b = 'b';
     
-    int arr[3];
+    
+    vector<int> v{1,2,3,4};//since c++11,普通变量可以类内初始化
+    static vector<int> vv;//static类变量不能类内初始化
 
-    Foo():arr{1,2,3}//member initialization
+//     //non-const-integral,non enum,non literaltype,不能in-class initialization
+//     const static int f[2];
+//     //non-static array ,since c++11 ,可以 in-class initialization
+//     //但是,必须指明size
+//     //在函数中,int z[]={1,2,3}是允许的
+//     int z[3]={1,2,3};
+
+    
+    //不能值初始化,Bar yourbar(33);因为这会被优先处理为一个函数声明
+    Bar yourbar = Bar(33);//直接初始化
+    Bar mybar{33};//初始化列表
+    
+    Fuu():array{1,2,3}//member initialization
     {
-        int x[]={1,2,3};//在函数中,不指明size的array initialization是允许的
+        //在函数中,可以从初始化列表中推断x的size
+        int x[]={1,2,3};
     };
 };
-const int Foo::f[2] = {1,2};
-vector<int> Foo::vv{3,5,6,7};
-const vector<int> Foo::vvv{8,8,8,8};
+// const int Foo::f[2] = {1,2};
+// vector<int> Foo::vv{3,5,6,7};
+// const vector<int> Foo::vvv{8,8,8,8};
 int main(int argc, char const *argv[])
 {
-    Foo f;
-    Foo::vv[0] = 9;//ok    
+    // Foo f;
+    // Foo::vv[0] = 9;//ok    
     //Foo::vvv[0] = 10; error
+    Fuu fu;
+    funfoo();
     return 0;
 }
