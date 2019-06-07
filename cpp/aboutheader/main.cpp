e80fe75546c52ad7a14f24348fad41763132084b
chen <1728533217en@gmail.com>
2019-06-07 20:46:12 +0800

    update

---
+++ cpp/aboutheader/main.cpp
@@ -0,0 +1,10 @@
+#include "headera.hpp"
+#include "usea.hpp"
+
+int main(int argc, char const *argv[])
+{
+  A a;
+  B b;
+  b.foo();
+  return 0;
+}
---
+++ cpp/aboutheader/usea.cpp
@@ -0,0 +1,12 @@
+// #include <iostream>
+#include "usea.hpp"
+
+
+
+void B::foo()
+{
+  // std::cout<<" i am fun foo in B"<<std::endl;
+  int a,b;
+  a=b=13;
+  int c = a*b;
+}
